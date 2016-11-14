12d6cce3840ed244a3e6c8a24622dd5e8daceb51
Mathias Axling <mathias.axling@cnet.se>
2016-11-14 12:52:47 +0100

    sync test

--- sync_power_analysis_day.py
+++ sync_power_analysis_day.py
@@ -18,28 +18,33 @@ def main():
  
     start_date = datetime.now() - timedelta(days=30)
     input = {
-    "energyhubid": "78:a5:04:ff:40:bb",
-    "starttime": datetime(2016,10,1,0,0,0),
-    "endtime": datetime.now() ,
+    "energyhubid": "78:a5:04:ff:40:bb", # •fe-ace-28 Örebrobostäder Granrisvägen     78:a5:04:ff:40:bb
+    "starttime": datetime(2016,11,7,0,0,0),
+    "endtime": datetime(2016,11,10,0,0,0),
     "userid": "testuser",
     "resultsid":"a1",
     "analysismodel":"POWERANALYSISDAY",
     }
     #print("\nRun the  aggregation for the input\n")
     #print(input)
-    #print("\nGet aggregate for the input\n")
-    #print(input)
     input["starttime"]=round_down_datetime(input["starttime"])
     input["endtime"]=round_up_datetime(input["endtime"])
+    print("\nGet aggregate for the input\n")
     print(input)
     # Get energy counter datafrom measurement DB
     aggr_data = pad.mdb_get_energy_counter_data_grouped(input)
+    print("\mdb_get_energy_counter_data_grouped(input)\n")
+    print(aggr_data)
     # Fetch the base load values
     base_values = base.get_base_load_values(input)
+    print("\get_base_load_values(input)\n")
+    print(base_values)
     # Calculate the averages
     hub_aggr = pad.get_energy_counter_aggregate(aggr_data, base_values)
+    print("\get_base_load_values(input)\n")
+    print(hub_aggr)
     input["data"]=list(hub_aggr)
-    print(input)
+    #print(input)
     # Store the result in the local analysis database
     #pad.mdb_insert_poweranalysisday_result(input)
     # Mark the job done
