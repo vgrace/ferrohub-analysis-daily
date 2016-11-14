7b619ab3eb35084ff28a986d53bf065060a717f4
Vivian Esquivias <v.esquivias@gmail.com>
2016-11-14 12:20:00 +0100

    go back to 2 hour offset

--- datetime_utilities.py
+++ datetime_utilities.py
@@ -6,7 +6,7 @@ from datetime import time
 
 # Constants
 no_of_seconds_in_a_day = 24 * 60 *60
-cest_offset_ms = 1000 * 60 * 60 * 1 # Two hours in milliseconds
+cest_offset_ms = 1000 * 60 * 60 * 2 # Two hours in milliseconds
 
 def round_down_datetime(datetime_value):
     return datetime.combine(datetime_value,time.min)
