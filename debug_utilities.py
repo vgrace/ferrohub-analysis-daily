b8f0475a737ce893b8382df2283843d67478d6df
Mathias Axling <mathias.axling@cnet.se>
2016-11-16 11:09:38 +0100

    debug

--- debug_utilities.py
+++ debug_utilities.py
@@ -5,6 +5,6 @@ def debug_print(is_debug,use_file, args):
     if is_debug:
         if use_file:
             with open("log"+date.today().strftime('%Y_%d_%m_%h')+".txt", "a") as myfile:
-                myfile.write(''.join(args))
+                myfile.write(''.join(map(str, args)))
         else:
             print(args)
