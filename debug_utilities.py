f6f273e85ae47473fe8a367b6547346b38a27655
Mathias Axling <mathias.axling@cnet.se>
2016-11-16 11:02:30 +0100

    debug

--- debug_utilities.py
+++ debug_utilities.py
@@ -2,7 +2,7 @@
 from datetime import date
 
 def debug_print(is_debug,use_file, args):
-    if isdebug:
+    if is_debug:
         if use_file:
             with open("log"+date.today().strftime('%Y_%d_%m_%h')+".txt", "a") as myfile:
                 myfile.write(args)
