b62c95c257edfa5c4315896cfa55f21d13f41a98
Mathias Axling <mathias.axling@cnet.se>
2016-11-16 11:07:21 +0100

    debug
    Signed-off-by: Mathias Axling <mathias.axling@cnet.se>

--- debug_utilities.py
+++ debug_utilities.py
@@ -5,6 +5,6 @@ def debug_print(is_debug,use_file, args):
     if is_debug:
         if use_file:
             with open("log"+date.today().strftime('%Y_%d_%m_%h')+".txt", "a") as myfile:
-                myfile.write(args)
+                myfile.write(''.join(args))
         else:
             print(args)
